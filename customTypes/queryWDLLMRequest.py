b5d83faa619a28741cd1e3397aab03794dee86f6
Eric Cambel <eric.cambel@ibm.com>
2024-03-25 19:05:18 -0400

    Added Watson Discovery api

--- customTypes/queryWDLLMRequest.py
+++ customTypes/queryWDLLMRequest.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, List
 
 class Moderations(BaseModel):
     hap_input: str = 'true'
@@ -36,18 +36,16 @@ class LLMParams(BaseModel):
     class Config:
         protected_namespaces = ()
 
-class queryLLMRequest(BaseModel):
-    llm_instructions: Optional[str] = Field(None, title="LLM Instructions", description="Instructions for LLM")
+class queryWDLLMRequest(BaseModel):
     question: str
-    es_index_name: str
-    es_index_text_field: Optional[str] = Field(default="body_content_field")
-    es_model_name: Optional[str] = Field(default=".elser_model_1")
-    es_model_text_field: Optional[str] = Field(default="ml.tokens")
+    project_id: str
+    collection_id: str
+    wd_version: Optional[str] = Field(default='2020-08-30')
+    wd_return_params: Optional[List[str]] = Field(default=["Title", "Text"], description="Params to pull from WD. Defaults Title and Text.")
+    llm_instructions: Optional[str] = Field(None, title="LLM Instructions", description="Instructions for LLM")
     num_results: Optional[str] = Field(default="5")
     llm_params: Optional[LLMParams] = LLMParams()
-    filters: Optional[Dict[str, Any]] = Field(None,
-        example={
-            "date": "2022-01-01",
-            "file_name": "test.pdf"
-        })
+    wd_document_names: Optional[List[str]] = Field(None,
+        example=["acme.pdf", "test.docx"]
+    )
 
